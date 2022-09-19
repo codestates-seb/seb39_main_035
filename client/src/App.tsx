656c9606c0e2d2dcc286f968fded7f3eaa20bce4
seul-dev <seul.dev@gmail.com>
2022-09-19 10:12:37 +0900

    🌱chore: page 삭제

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import styled from 'styled-components';
 import GlobalStyle from './GlobalStyle';
 import Header from './components/Header';
-import Landing from './pages/Landing';
 
 function App() {
   return (
@@ -11,9 +9,6 @@ function App() {
       <BrowserRouter>
         <GlobalStyle />
         <Header />
-        <Routes>
-          <Route path='/' element={<Landing />} />
-        </Routes>
       </BrowserRouter>
     </>
   );
