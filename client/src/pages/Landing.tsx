3de88b032ebfd7f680e6fc90b987e29d0e570973
StarryPro <hansunguk6178@gmail.com>
2022-10-04 17:58:44 +0900

    💄 design: 랜딩 페이지 서비스 이름 수정

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,25 +1,19 @@
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
-import Button from '../components/Button';
 
 export const Container = styled.section`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  padding: 1.5rem 2rem;
-  border-radius: 0.25rem;
-  box-shadow: rgb(0 0 0 / 20%) 0px 0px 4px 0px;
-  width: 300px;
 
   h1 {
-    /* height: 100px; */
     display: flex;
+    line-height: 7.5rem;
   }
 
   h1 span {
     position: relative;
-    top: 20px;
+    top: 5px;
     display: inline-block;
     animation: bounce 0.9s ease infinite alternate;
     font-family: 'RIDIBatang', cursive;
@@ -54,7 +48,7 @@ export const Container = styled.section`
 
   @keyframes bounce {
     100% {
-      top: -20px;
+      top: -5px;
       text-shadow: 0 1px 0 #ffbfc5, 0 2px 0 #ffbfc5, 0 3px 0 #ffbfc5,
         0 4px 0 #ffbfc5, 0 5px 0 #ffbfc5, 0 6px 0 #ffbfc5, 0 7px 0 #ffbfc5,
         0 8px 0 #ffbfc5, 0 9px 0 #ffbfc5, 0 50px 25px rgba(0, 0, 0, 0.2);
@@ -62,37 +56,20 @@ export const Container = styled.section`
   }
 `;
 
-// 로그인, 회원가입 버튼
-export const SignBtn = styled.div`
-  display: flex;
-  justify-content: space-between;
-  a {
-    text-decoration: none;
-    color: black;
-  }
-`;
-
 const Landing = () => {
-  const navigate = useNavigate();
   return (
     <Container>
       <h1>
-        <span>S</span>
-        <span>E</span>
-        <span>O</span>
-        <span>L</span>
-        <span>L</span>
-        <span>E</span>
-        <span>M</span>
+        <span>나</span>
+        <span>만</span>
+        <span>의</span>
       </h1>
-      <SignBtn>
-        <Button color='mint' onClick={() => navigate('/members/sign-in')}>
-          로그인
-        </Button>
-        <Button color='mint' onClick={() => navigate('/members/sign-up')}>
-          회원가입
-        </Button>
-      </SignBtn>
+      <h1>
+        <span>작</span>
+        <span>은</span>
+        <span>설</span>
+        <span>렘</span>
+      </h1>
     </Container>
   );
 };
