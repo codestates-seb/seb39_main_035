424d811a7689cfc55d5b76c39face9a8ee3e70f7
seul-dev <seul.dev@gmail.com>
2022-10-04 14:38:56 +0900

    🎨 style: random memo 컴포넌트 스타일 추가

--- client/src/components/RandomMemo.tsx
+++ client/src/components/RandomMemo.tsx
@@ -17,8 +17,10 @@ const RandomMemo = () => {
 
   return (
     <Wrapper>
-      <p>{date}</p>
       <Viewer initialValue={memo.memoContent} />
+      <MemoInfo>{date}</MemoInfo>
+      <MemoInfo>{memo.memoBookPage + 'p'}</MemoInfo>
+      <MemoInfo>{memo.memoType}</MemoInfo>
     </Wrapper>
   );
 };
@@ -26,9 +28,21 @@ const RandomMemo = () => {
 export default RandomMemo;
 
 // 클릭 시 해당 책 상세페이지로 가기(bookId가 없으면 못가는거구만! )
-// 책, 메모페이지
+// 책, 메모북 페이지, 타입
+// 포스트잇 모양..!
 
 const Wrapper = styled.div`
-  background-color: white;
+  background-color: var(--light-blue);
   padding: 1rem;
+  border-radius: 5px;
+  .toastui-editor-contents {
+    font-size: 16px;
+    font-family: 'RIDIBatang';
+  }
+`;
+
+const MemoInfo = styled.div`
+  text-align: right;
+  margin-bottom: 5px;
+  font-size: 13px;
 `;
